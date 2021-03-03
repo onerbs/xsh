16774eac217a173c086368ea10255f2ec58b27b6
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-03 00:06:53 -0600

    doc(book): Refactoring ...

--- src/doc/book.v
+++ src/doc/book.v
@@ -14,37 +14,25 @@ pub:
 	sheets []Sheet
 }
 
-pub fn book(root string) Book {
+pub fn book(origin string) Book {
 	mut shelve := []Book{}
 	mut sheets := []Sheet{}
-	for file in get_files(root) {
+	for file in get_files(origin) {
 		if os.is_dir(file) {
 			shelve << book(file)
 		} else {
 			sheets << sheet(file)
 		}
 	}
-	return Book{
-		origin: root
-		name: os.base(root) + '/'
-		books: books
-		sheets: sheets
-	}
+	name := os.base(origin) + '/'
+	return Book{origin, name, shelve, sheets}
 }
 
 fn get_files(root string) []string {
-	files := os.ls(root) or { []string{cap: 0} }
+	files := os.ls(root) or { []string{len: 0} }
 	return files.filter(valid_file).map('$root/$it')
 }
 
 fn valid_file(name string) bool {
-	return if name.starts_with('_') {
-		false
-	} else if name.ends_with('_.sh') {
-		false
-	} else if name == '.git' {
-		false
-	} else {
-		true
-	}
+	return !(name.starts_with('_') || name.ends_with('_.sh') || name == '.git')
 }
