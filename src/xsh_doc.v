48824b277c162a691d689c0e8736e51654fe89a1
AAAA <c326e738e52c5469f518071e@yahoo.com>
2021-03-17 23:53:15 -0600

    tool(doc): feat: Simplify logic

--- src/xsh_doc.v
+++ src/xsh_doc.v
@@ -8,33 +8,14 @@ import doc
 import plu { fail }
 
 //
-// Usage: xsh_doc <lib> [<fun>]
+// Usage: xsh_doc <lib> [<name>]
 //
 
 fn main() {
 	args := plu.need_args(1) or { exit(fail(err.msg)) }
-
-	lib_name := args.first()
-	fun_name := if args.len > 1 { args[1] } else { '' }
-
-	sheets := doc.get_sheets_by_name(lib_name) or { exit(fail(err.msg)) }
-
-	if fun_name.len > 0 {
-		for s in sheets {
-			target := s.get_entes().find(fun_name) or { continue }
-			plu.set_title(s.origin)
-			println(target)
-			exit(0)
-		}
-		exit(not_found(fun_name, lib_name))
-	} else {
-		for s in sheets {
-			plu.set_title(s.origin)
-			println(s.get_entes().map('$it').join('\n'))
-		}
+	name := if args.len > 1 { args[1] } else { '' }
+	mut sheets := doc.get_sheets_by_name(args[0])
+	for mut sheet in sheets {
+		println(sheet.parse().peek(name))
 	}
-}
-
-fn not_found(fun string, lib string) int {
-	return fail('the function "$fun" was not found in lib "$lib"')
 }
